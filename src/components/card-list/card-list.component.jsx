a7074fd3a683f398eb8eeabb80395f8566f47217
mrkakadiya1603 <107118287+mrkakadiya1603@users.noreply.github.com>
2022-11-29 15:17:45 +0530

    adding files for github pages

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import "./card-list.style.css";
+import Card from "../card/card.component";
+
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
+
+
+
+export default CardList;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,31 @@
+import "./card.style.css";
+
+function MonsterCard({ monster: { id, name, email } }) {
+  return (
+    <div className="card-container">
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?size=180x180&set=set2`}></img>
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+}
+
+// class MonsterCard extends Component {
+//   render() {
+//     const { id, name, email } = this.props.monster;
+
+//     const srcUrl = `https://robohash.org/${id}?size=180x180&set=set2`;
+
+//     return (
+//       <div className="card-container">
+//         <img alt={`monster ${name}`} src={srcUrl}></img>
+//         <h2>{name}</h2>
+//         <p>{email}</p>
+//       </div>
+//     );
+//   }
+// }
+
+export default MonsterCard;
